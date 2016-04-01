065821f6b8e37034a81b700bcb536a370ed206ce
Friedrik <friedrikbosker@gmail.com>
2016-04-01 11:53:49 +0200

    versie 2

---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Friedrik on 1-4-2016.
+ */
+public class Runner {
+
+    public static void main (String[] args){
+        Simulator sim = new Simulator();
+        sim.run();
+
+    }
+}
