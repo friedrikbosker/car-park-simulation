44c95b6c9fd2b6292b8c9e8b2836bd147005af5e
Friedrik <friedrikbosker@gmail.com>
2016-04-01 12:26:52 +0200

    versie 3

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,26 @@
 /**
  * Created by Friedrik on 1-4-2016.
  */
+import java.awt.*;
+        import java.awt.event.*;
+                import javax.swing.*;
+
 public class Runner {
 
     public static void main (String[] args){
-        Simulator sim = new Simulator();
-        sim.run();
+         JFrame frame = new JFrame("Car park menu");
+        Container contentPane = frame.getContentPane();
+
+        JMenuBar menubar = new JMenuBar();
+        frame.setJMenuBar(menubar);
+        JMenu fileMenu = new JMenu("Run");
+        menubar.add(fileMenu);
+        JMenuItem runsimulation = new JMenuItem("Run Simulation");
+        fileMenu.add(runsimulation);
+
+
+        frame.pack();
+        frame.setVisible(true);
 
     }
 }
