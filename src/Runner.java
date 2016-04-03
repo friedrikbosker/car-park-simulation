28ff3e20ee193e8666c07022c1c651116c63ff49
Friedrik <friedrikbosker@gmail.com>
2016-04-03 23:24:29 +0200

    versie 3.1

--- src/Runner.java
+++ src/Runner.java
@@ -10,15 +10,20 @@ public class Runner {
     public static void main (String[] args){
          JFrame frame = new JFrame("Car park menu");
         Container contentPane = frame.getContentPane();
-
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
         JMenu fileMenu = new JMenu("Run");
         menubar.add(fileMenu);
         JMenuItem runsimulation = new JMenuItem("Run Simulation");
+        runsimulation.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Simulator simulatin = new Simulator();
+                simulatin.run();
+            }
+        });
         fileMenu.add(runsimulation);
 
-
+        frame.getContentPane().add(runsimulation);
         frame.pack();
         frame.setVisible(true);
 
