7db33a95352482d17facab1c2de67d3c9d6bc600
Friedrik <friedrikbosker@gmail.com>
2016-04-04 15:42:33 +0200

    versie 3.2

--- src/Runner.java
+++ src/Runner.java
@@ -7,25 +7,44 @@ import java.awt.*;
 
 public class Runner {
 
-    public static void main (String[] args){
-         JFrame frame = new JFrame("Car park menu");
+    public static void main (String[] args) {
+        JFrame frame = new JFrame("Car park menu");
+
         Container contentPane = frame.getContentPane();
+
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
+
         JMenu fileMenu = new JMenu("Run");
         menubar.add(fileMenu);
-        JMenuItem runsimulation = new JMenuItem("Run Simulation");
-        runsimulation.addActionListener(new ActionListener() {
+
+        JMenuItem startsimulation = new JMenuItem("Create Simulator");
+        startsimulation.addActionListener(new ActionListener() {
+             @Override
             public void actionPerformed(ActionEvent e) {
-                Simulator simulatin = new Simulator();
-                simulatin.run();
+                Simulator sim = new Simulator();
+
+
+            }
+        });
+        fileMenu.add(startsimulation);
+        frame.getContentPane().add(startsimulation);
+
+        JMenuItem runsimulation = new JMenuItem("Run Simulator");
+        runsimulation.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                sim.run();
             }
         });
         fileMenu.add(runsimulation);
-
         frame.getContentPane().add(runsimulation);
+
         frame.pack();
         frame.setVisible(true);
 
     }
+
+
 }
